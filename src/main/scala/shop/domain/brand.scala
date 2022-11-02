d85a5819dac08d7d557f66a2a625437d395769eb
EstebanMarin <estebanmarin002@gmail.com>
2022-11-02 15:31:52 -0500

    pre-split

--- src/main/scala/shop/domain/brand.scala
+++ src/main/scala/shop/domain/brand.scala
@@ -2,7 +2,10 @@ package shop.domain
 
 import java.util.UUID
 import io.estatico.newtype.macros.newtype
+import eu.timepit.refined.types.string._
 
 object brand {
   @newtype case class BrandId(value: UUID)
+  @newtype case class BrandName(value: NonEmptyString)
+  case class Brand(uuid: BrandId, name: BrandName)
 }
