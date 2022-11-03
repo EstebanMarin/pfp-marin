c404cff14feaa762409dcec56e9527cfb512600b
EstebanMarin <estebanmarin002@gmail.com>
2022-11-03 15:23:24 -0500

    defined program

--- modules/core/src/main/scala/shop/domain/orders.scala
+++ modules/core/src/main/scala/shop/domain/orders.scala
@@ -6,10 +6,13 @@ import shop.domain.cart._
 import io.estatico.newtype.macros.newtype
 import java.util.UUID
 import squants.market.Money
+import scala.util.control.NoStackTrace
 
 object orders {
   @newtype case class OrderID(uuid: UUID)
   @newtype case class PaymentID(uuid: UUID)
 
   case class Order(id: OrderID, pid: PaymentID, items: Map[ItemID, Quantity], total: Money)
+
+  case object EmptyCartError extends NoStackTrace
 }
--- modules/core/src/main/scala/shop/domain/programs/Checkout.scala
+++ modules/core/src/main/scala/shop/domain/programs/Checkout.scala
@@ -1,22 +1,33 @@
 package shop.domain.programs
 
-import cats.Monad
+import cats._
+import cats.implicits._
+import cats.data._
+
 import shop.domain.http.PaymentClient
 import shop.services.ShoppingCart
 import shop.services.Orders
 import shop.domain.authentication._
 import shop.domain.checkout._
 import shop.domain.orders._
+import shop.domain.payment._
+import shop.domain.cart._
 
-final case class Checkout[F[_]: Monad](
+final case class Checkout[F[_]: MonadThrow](
     payments: PaymentClient[F],
     cart: ShoppingCart[F],
     orders: Orders[F]
 ) {
-  def process(usedId: UserId, card: Card): F[OrderID] =
-    // for {
-    // //   c <- cart.get(usedId)
-    //   //   pid <- payments.process(c.item)
-    // } yield ???
-    ???
+
+  private def ensureNonEmpty[A](xs: List[A]): F[NonEmptyList[A]] =
+    MonadThrow[F].fromOption(NonEmptyList.fromList(xs), EmptyCartError)
+
+  def process(userId: UserId, card: Card): F[OrderID] = {
+    for {
+      c: shop.domain.cart.CartTotal <- cart.get(userId)
+      its: NonEmptyList[CartItem]   <- ensureNonEmpty(c.items)
+      pid: PaymentID                <- payments.process(Payment(userId, c.total, card))
+      oid: OrderID                  <- orders.create(userId, pid, its, c.total)
+    } yield oid
+  }
 }
