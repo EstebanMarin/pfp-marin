fd97fb89241962e58910dbd749d196f963566fb7
EstebanMarin <estebanmarin002@gmail.com>
2022-11-17 16:25:30 -0500

    working with reddis!

--- modules/core/src/main/scala/shop/resources/AppResources.scala
+++ modules/core/src/main/scala/shop/resources/AppResources.scala
@@ -4,7 +4,6 @@ import cats.effect.std.Console
 import cats.effect._
 import cats.syntax.all._
 import dev.profunktor.redis4cats.{ Redis, RedisCommands }
-import eu.timepit.refined.auto._
 import fs2.io.net.Network
 import natchez.Trace.Implicits.noop
 import org.typelevel.log4cats.Logger
@@ -13,7 +12,10 @@ import skunk.codec.text._
 import skunk.implicits._
 import dev.profunktor.redis4cats.effect.MkRedis
 
-sealed abstract class AppResources[F[_]](val postgres: Resource[F, Session[F]])
+sealed abstract class AppResources[F[_]](
+    val postgres: Resource[F, Session[F]],
+    val redis: RedisCommands[F, String, String]
+)
 
 object AppResources {
   def make[F[_]: Concurrent: Network: Console: Logger: MkRedis]: Resource[F, AppResources[F]] = {
@@ -47,9 +49,6 @@ object AppResources {
     def mkRedisResource: Resource[F, RedisCommands[F, String, String]] =
       Redis[F].utf8("redis://localhost").evalTap(redis => checkRedisConnection(redis))
 
-    for {
-      postgres: Resource[F, Session[F]]       <- mkPostgreSqlResource
-      redis: RedisCommands[F, String, String] <- mkRedisResource
-    } yield new AppResources[F](postgres) {}
+    (mkPostgreSqlResource, mkRedisResource).parMapN(new AppResources[F](_, _) {})
   }
 }
